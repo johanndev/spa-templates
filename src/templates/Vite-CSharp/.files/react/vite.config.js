3faf1fdd6f419dba9c051480d323bb9c4a92202d
johanndev <10048453+johanndev@users.noreply.github.com>
2021-11-11 14:01:36 +0100

    Update vite templates to 2.6.4

--- src/templates/Vite-CSharp/.files/react/vite.config.js
+++ src/templates/Vite-CSharp/.files/react/vite.config.js
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { readFileSync } from 'fs'
-import { certFilePath, keyFilePath } from './aspnetcore-https'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: readFileSync(keyFilePath),
-      cert: readFileSync(certFilePath)
-    },
-    port: 5002,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:5001/',
-        changeOrigin: true,
-        secure: true
-      }
-    }
-  }
+  plugins: [react()]
 })
--- src/templates/Vite-CSharp/.files/svelte-ts/vite.config.js
+++ src/templates/Vite-CSharp/.files/svelte-ts/vite.config.js
@@ -1,24 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { readFileSync } from 'fs'
-import { certFilePath, keyFilePath } from './aspnetcore-https.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    https: {
-      key: readFileSync(keyFilePath),
-      cert: readFileSync(certFilePath)
-    },
-    port: 5002,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'https://localhost:5001/',
-        changeOrigin: true,
-        secure: true
-      }
-    }
-  }
+  plugins: [svelte()]
 })
